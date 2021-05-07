c0400c8ee2db836067297c2edf853aa018fbbb96
hector97i <43556352+hector97i@users.noreply.github.com>
2021-05-07 11:14:04 -0500

    Added try except to prediciton

--- main.py
+++ main.py
@@ -98,48 +98,52 @@ def add_nomask():
 def mask_detection(frame, image_flag):
     # Capture frame-by-frame
     # ret, frame = video_capture.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,
-                                        scaleFactor=1.1,
-                                        minNeighbors=5,
-                                        minSize=(60, 60),
-                                        flags=cv2.CASCADE_SCALE_IMAGE)
-    faces_list=[]
-    preds=[]
-    for (x, y, w, h) in faces:
-        face_frame = frame[y:y+h,x:x+w]
-        face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
-        face_frame = cv2.resize(face_frame, (224, 224))
-        face_frame = img_to_array(face_frame)
-        face_frame = np.expand_dims(face_frame, axis=0)
-        face_frame =  preprocess_input(face_frame)
-        faces_list.append(face_frame)
-        if len(faces_list)>0:
-            preds = model.predict(face_frame)
+    try:
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        faces = faceCascade.detectMultiScale(gray,
+                                            scaleFactor=1.1,
+                                            minNeighbors=5,
+                                            minSize=(60, 60),
+                                            flags=cv2.CASCADE_SCALE_IMAGE)
+        faces_list=[]
+        preds=[]
+        for (x, y, w, h) in faces:
+            face_frame = frame[y:y+h,x:x+w]
+            face_frame = cv2.cvtColor(face_frame, cv2.COLOR_BGR2RGB)
+            face_frame = cv2.resize(face_frame, (224, 224))
+            face_frame = img_to_array(face_frame)
+            face_frame = np.expand_dims(face_frame, axis=0)
+            face_frame =  preprocess_input(face_frame)
+            faces_list.append(face_frame)
+            if len(faces_list)>0:
+                preds = model.predict(face_frame)
+            else:
+                return "NO FACES DETECTED"
+            for pred in preds:
+                (mask, withoutMask) = pred
+            label = "Mask" if mask > withoutMask else "No Mask"
+            # color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+            label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+            if mask > withoutMask:
+                print('MASK')
+                # add_mask()
+
+            else:
+                print('NO MASK')
+                # add_nomask()
+
+            # cv2.putText(frame, label, (x, y- 10),
+            #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+
+            # cv2.rectangle(frame, (x, y), (x + w, y + h),color, 2)
+
+        if image_flag:
+            return frame
         else:
-            label = 'NO FACES DETECTED'
-        for pred in preds:
-            (mask, withoutMask) = pred
-        label = "Mask" if mask > withoutMask else "No Mask"
-        # color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
-        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
-        if mask > withoutMask:
-            print('MASK')
-            # add_mask()
-
-        else:
-            print('NO MASK')
-            # add_nomask()
-
-        # cv2.putText(frame, label, (x, y- 10),
-        #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
-
-        # cv2.rectangle(frame, (x, y), (x + w, y + h),color, 2)
-
-    if image_flag:
-        return frame
-    else:
-        return label
+            return label
+            
+    except Exception as e:
+        print(e)
 
 if __name__ == '__main__':
 
