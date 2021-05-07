9824eaedaa03b88b3a0527f42a85c60961e850b4
hector97i <43556352+hector97i@users.noreply.github.com>
2021-05-07 11:04:56 -0500

    Fixed bug in return value.

--- main.py
+++ main.py
@@ -98,7 +98,6 @@ def add_nomask():
 def mask_detection(frame, image_flag):
     # Capture frame-by-frame
     # ret, frame = video_capture.read()
-    label = 'NO FACES DETECTED'
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = faceCascade.detectMultiScale(gray,
                                         scaleFactor=1.1,
@@ -117,24 +116,25 @@ def mask_detection(frame, image_flag):
         faces_list.append(face_frame)
         if len(faces_list)>0:
             preds = model.predict(face_frame)
+        else:
+            label = 'NO FACES DETECTED'
         for pred in preds:
             (mask, withoutMask) = pred
         label = "Mask" if mask > withoutMask else "No Mask"
-        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+        # color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
         label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
         if mask > withoutMask:
             print('MASK')
             # add_mask()
-            # ser.write(True)
+
         else:
             print('NO MASK')
             # add_nomask()
-            # ser.write(False)
 
-        cv2.putText(frame, label, (x, y- 10),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        # cv2.putText(frame, label, (x, y- 10),
+        #             cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
 
-        cv2.rectangle(frame, (x, y), (x + w, y + h),color, 2)
+        # cv2.rectangle(frame, (x, y), (x + w, y + h),color, 2)
 
     if image_flag:
         return frame
